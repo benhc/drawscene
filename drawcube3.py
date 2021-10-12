6bcec4185a58a0a2e76b229cf9913b32fd35cc28
benhc <30448812+benhc@users.noreply.github.com>
2021-10-12 20:26:16 +0100

    Use pipenv (#1)
    Co-authored-by: Ben Hunter-Craig <benhunter-craig@Bens-MacBook-Pro.local>

--- drawcube3.py
+++ drawcube3.py
@@ -14,12 +14,12 @@ from calibrate_camera import *
 image = 'fountain'
 
 if image == 'fountain':
-    #Camera params for the fountain
+    # Camera params for the fountain
     K = np.array([[2759.48, 0, 1520.69, 0, 2764.16, 1006.81, 0, 0, 1]]).reshape(3, 3)
     dist = np.array([0.0, 0.0, 0.0, 0.0, 0.0]).reshape(1, 5)
     # Import images as greyscale
-    img1 = cv2.imread('0005.jpg',0)  #left image
-    img2 = cv2.imread('0004.jpg',0) #right image
+    img1 = cv2.imread('0005.jpg',0)  # left image
+    img2 = cv2.imread('0004.jpg',0)  # right image
 
 else:
     try:
@@ -32,8 +32,8 @@ else:
     # Load image pair to perform reconstruction on
     path = "/examplepath/"
     os.chdir(path)
-    img1 = cv2.imread('im_1.JPG',0)  #left image
-    img2 = cv2.imread('im_2.JPG',0) #right image
+    img1 = cv2.imread('im_1.JPG',0)  # left image
+    img2 = cv2.imread('im_2.JPG',0)  # right image
     # Correct if the image is portrait
     if img1.shape[0]<img1.shape[1]:
         K[0][0], K[1][1] = K[1][1], K[0][0]
@@ -67,7 +67,8 @@ kp1 = kp1[filtcond].reshape(-1, 2)
 kp2 = kp2[filtcond].reshape(-1, 2)
 draw_flow(img1, kp1, kp2)
 
-# Find the essential matrix# 
+# Find the essential matrix
+# Find the essential matrix
 E, mask = cv2.findEssentialMat(kp1, kp2, K, cv2.RANSAC, threshold = 0.5)
 kp1 = kp1[mask.ravel() == 1]
 kp2 = kp2[mask.ravel() == 1]
@@ -76,12 +77,12 @@ draw_flow(img1, kp1, kp2)
 # take svd
 U, s, Vt = np.linalg.svd(E)
 print(s)
-#orthogonal matrix.
+# orthogonal matrix.
 W = np.array([[0.0, -1.0, 0.0],
               [1.0, 0.0, 0.0], 
               [0.0, 0.0, 1.0]])
 
-#4 poss solutions. 
+# 4 poss solutions.
 R=[np.dot(U, np.dot(W, Vt)), np.dot(U, np.dot(np.transpose(W), Vt))]
 T=[U[:, 2], -U[:, 2]]
 
@@ -92,7 +93,7 @@ print(K)
 P1 = np.dot(K, np.hstack((np.eye(3), np.zeros((3, 1)))))
 P2 = np.dot(K, np.hstack((R[1], T[1].reshape(-1, 1))))
  
-## Perform image rectification
+# Perform image rectification
 R1, R2, Pa, Pb, Q, roi1, roi2 = cv2.stereoRectify(K, dist, K, dist, img2.shape[:2][::-1], R[0], T[0])
 print(P1, P2)
 map1x, map1y = cv2.initUndistortRectifyMap(K, dist, R1, Pa[:3][:3], img1.shape[:2][::-1], cv2.CV_32F)
@@ -111,7 +112,6 @@ cv2.namedWindow('image',cv2.WINDOW_NORMAL)
 cv2.resizeWindow('image', 600,600)
 cv2.imshow('image', rectimg)
 cv2.waitKey()
-###########
 
 homimgpoints = np.transpose(cv2.triangulatePoints(P1, P2, np.transpose(kp1), np.transpose(kp2), None))
 
@@ -138,3 +138,30 @@ ax.scatter(imgpoints[:,0], imgpoints[:,1], imgpoints[:,2],marker = 'o', edgecolo
 plt.show()
 
 
+def is_in_front(kp1, kp2, R, T, K):
+    """
+    Function to resolve ambguity in projection matrix. kp1 and kp2 contain the
+    matched keypoints in images 1 and 2 respectively
+    """
+
+    # Use R and T to find the 3d coordinates and then check if they have the right
+    # coordinates to be in front of the camera
+    # Make the projection matrices
+    P1 = np.dot(K, np.hstack((np.eye(3), np.zeros((3, 1)))))
+    P2 = np.dot(K, np.hstack((R, T.reshape(3, 1))))
+    # Triangulate the world coordinate points
+    homimgpoints = np.transpose(cv2.triangulatePoints(P1,P2, np.transpose(kp1), np.
+    transpose(kp2), None))
+    imgpoints = homimgpoints[:, :3] / np.repeat(homimgpoints[:, 3], 3).reshape(-1, 3)
+
+    # Put the world coordinates into camera centred coordinates for each camera.
+    pts1 = [i for i in imgpoints]
+    pts2 = [np.reshape(np.dot(R,i.reshape(3, 1))+T.reshape(3, 1), (1,3) ) [0] for i in
+    imgpoints]
+    # If the camera cant see it i .e −ve Z value, put False in the array
+    viewpts1 = np.reshape([True if p[2]>0 else False for p in pts1 ], (-1, 1))
+    viewpts2 = np.reshape([True if p[2]>0 else False for p in pts2 ], (-1, 1))
+
+    # Count the number of points that can’t be seen
+    viewcond = viewpts1 * viewpts2
+    return sum(viewcond)
